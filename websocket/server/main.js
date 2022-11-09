a032362d9e65325559867a386bca5cf614c484c4
Luis David Cornejo <luisdavidcorbra24@gmail.com>
2022-11-08 19:01:52 -0500

    Servidor
    implementación del servidor

---
+++ websocket/server/main.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+//ARREGLO QUE CONTIENE TODOS LOS MENSAJES ENVIADOS A TRAVÉS DE WEBSOCKET
+var messages = [{
+        id: 1,
+        text: "Hola soy un mensaje desde el servidor",
+        author: "Servidor WebSocket"
+}];
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    console.log('Alguien se ha conectado con Sockets');
+
+    //ENVIAR MENSAJE DESDE SERVIDOR
+    socket.emit('messages', messages);
+
+    //ESCUCHAR MENSAJE DESDE CLIENTE
+    socket.on('new-message', function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+
+        //AQUI SE MANDA A IMPRIMIR A LA IMPRESORA (data)
+    });
+});
+
+server.listen(8080, function(){
+    console.log("Servidor corriendo en http://localhost:8080/hello");
+});
